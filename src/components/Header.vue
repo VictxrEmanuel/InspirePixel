5c94d17fcd2d03f09bf9fcba9eaedec6adad0267
victxremanuel <victor.ribeiro090903@gmail.com>
2025-11-28 10:35:56 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+import logo from "../assets/logo.png"
+</script>
+
+<template>
+<header>
+    <img :src="logo" alt="">
+    <nav>
+        <li><a href="">
+            <Icon icon="material-symbols:search"/>
+        </a></li>
+        <li><a href="">Ínicio</a></li>
+        <li><a href="">Galeria</a></li>
+        <li><a href="">Favoritos</a></li>
+        <li><a href="">
+            <Icon icon="material-symbols:person"/>
+        </a></li>
+    </nav>
+</header>
+</template>
+
+<style scoped lang="scss">
+</style>
