01c4419e7ce366b56a7da33ca1ea44aff057067c
victxremanuel <victor.ribeiro090903@gmail.com>
2025-12-03 09:43:11 -0300

    atualizações

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,12 +4,41 @@
 
 <template>
     <section class="banner">
-        <h1>Onde a paisagem vira
+        <div>
+            <h1>Onde a paisagem vira
             <span>inspiração</span> .</h1>
-        <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+            <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+           
+        </div>
         <img :src="fotografo" alt="" >
     </section>
 </template>
 
 <style scoped lang="scss">
+.banner{
+    // border: solid;
+    display: flex;
+    margin:2rem;
+    justify-content: space-between
+
+    h1{
+        font-size: 4rem;
+        
+
+    }
+    span{
+            color: #E1306C;
+            font-size: 4.5rem;
+        }
+
+    div{
+        //border: solid;
+        width:50%;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    
+}
 </style>
